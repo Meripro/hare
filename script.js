2be172a4f9138cba3519a46ee809bcd01ce83247
Meron Yohannes <myohannes268@gmail.com>
2022-10-04 01:14:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// Array of Objects
+const quiz = [
+    {
+        q:'where was Jesus born?',
+        option:['Addis-Abeba','Bethlehem','Jerusalem','none'],
+        answer:1
+    },
+    {
+        q:'How many fish did Jesus need to feed the 5000 people?',
+        option:['2','1','5000','no answer'],
+        answer:0
+    },
+    {
+        q:'Who assassinated John the Baptist?',
+        option:['king David','Menelik', 'Herod Antipas', 'no body'],
+        answer:2
+    },
+    {
+        q:'Abel killed cain. say true or false?',
+        option:['true','false'],
+        answer:1
+    },
+    {
+        q:'What was the name of the Wise King?',
+        option:['solomon','noah','Adam','Ermias'],
+        answer:0
+    },
+    {
+        q:'Who approached Jesus in the middle of the night?',
+        option:['Alazar','Martha','Luke','Nicodemus'],
+        answer:3
+    }
+]
